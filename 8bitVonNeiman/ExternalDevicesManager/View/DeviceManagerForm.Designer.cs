23fa71b16df1711f4602cd3cc5d681e3417cb780
Alexander Zuev <zuev08@gmail.com>
2018-12-24 06:23:42 +0300

    Добавлена кнопка "Дисплей", внешний вид соответствует КР

--- 8bitVonNeiman/ExternalDevicesManager/View/DeviceManagerForm.Designer.cs
+++ 8bitVonNeiman/ExternalDevicesManager/View/DeviceManagerForm.Designer.cs
@@ -23,36 +23,48 @@
 		/// the contents of this method with the code editor.
 		/// </summary>
 		private void InitializeComponent() {
-			this.addDeviceButton = new System.Windows.Forms.Button();
-			this.SuspendLayout();
-			// 
-			// addDeviceButton
-			// 
-			this.addDeviceButton.Location = new System.Drawing.Point(85, 12);
-			this.addDeviceButton.Name = "addDeviceButton";
-			this.addDeviceButton.Size = new System.Drawing.Size(149, 23);
-			this.addDeviceButton.TabIndex = 0;
-			this.addDeviceButton.Text = " Подключить клавиатуру";
-			this.addDeviceButton.UseVisualStyleBackColor = true;
-			this.addDeviceButton.Click += new System.EventHandler(this.addDeviceButton_Click);
-			// 
-			// DeviceManagerForm
-			// 
-			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
-			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.ClientSize = new System.Drawing.Size(324, 91);
-			this.Controls.Add(this.addDeviceButton);
-			this.Location = new System.Drawing.Point(0, 200);
-			this.Name = "DeviceManagerForm";
-			this.StartPosition = System.Windows.Forms.FormStartPosition.Manual;
-			this.Text = "Диспетчер внешних устройств";
-			this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.DeviceManagerForm_FormClosed);
-			this.ResumeLayout(false);
+            this.addDeviceButton = new System.Windows.Forms.Button();
+            this.addDisplayButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // addDeviceButton
+            // 
+            this.addDeviceButton.Location = new System.Drawing.Point(85, 12);
+            this.addDeviceButton.Name = "addDeviceButton";
+            this.addDeviceButton.Size = new System.Drawing.Size(149, 23);
+            this.addDeviceButton.TabIndex = 0;
+            this.addDeviceButton.Text = "Клавиатура";
+            this.addDeviceButton.UseVisualStyleBackColor = true;
+            this.addDeviceButton.Click += new System.EventHandler(this.addDeviceButton_Click);
+            // 
+            // addDisplayButton
+            // 
+            this.addDisplayButton.Location = new System.Drawing.Point(85, 41);
+            this.addDisplayButton.Name = "addDisplayButton";
+            this.addDisplayButton.Size = new System.Drawing.Size(149, 24);
+            this.addDisplayButton.TabIndex = 1;
+            this.addDisplayButton.Text = "Дисплей";
+            this.addDisplayButton.UseVisualStyleBackColor = true;
+            // 
+            // DeviceManagerForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(324, 91);
+            this.Controls.Add(this.addDisplayButton);
+            this.Controls.Add(this.addDeviceButton);
+            this.Location = new System.Drawing.Point(0, 200);
+            this.Name = "DeviceManagerForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.Manual;
+            this.Text = "Диспетчер внешних устройств";
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.DeviceManagerForm_FormClosed);
+            this.ResumeLayout(false);
 
 		}
 
 		#endregion
 
 		private System.Windows.Forms.Button addDeviceButton;
-	}
+        private System.Windows.Forms.Button addDisplayButton;
+    }
 }
